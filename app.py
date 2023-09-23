51863883be5e81c28b86ee8f8149fa86943dedbf
Lize Shao <ls94@rice.edu>
2023-09-23 15:08:44 -0500

    chat done

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, jsonify, render_template
+import openai
+
+app = Flask(__name__)
+openai.api_key = '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'
+
+@app.route('/')
+def index():
+    return render_template('main_webpage.html')
+
+
+@app.route('/get_answer', methods=['POST'])
+def get_answer():
+    question = request.json.get('question')
+    response = openai.Completion.create(engine="text-davinci-003", prompt=question, max_tokens=150)
+    answer = response.choices[0].text.strip()
+    return jsonify(answer=answer)
+
+if __name__ == '__main__':
+    app.run(debug=True)
