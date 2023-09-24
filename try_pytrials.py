f1150f30164c3c603763b55482dff7da177f9399
Your Name <yifan21lu@gmail.com>
2023-09-24 12:45:06 +0800

    js updated.

--- try_pytrials.py
+++ try_pytrials.py
@@ -2,16 +2,16 @@ from pytrials.client import ClinicalTrials
 import json
 
 ct = ClinicalTrials()
-# res = ct.get_full_studies(search_expr="Coronavirus+COVID", max_studies=1)
+res2 = ct.get_full_studies(search_expr="NCT01830283", max_studies=1)
 res = ct.get_study_fields(
     search_expr="Coronavirus+COVID OR Chickenpox ",
-    fields=["NCTId", "Condition", "BriefTitle", "OverallStatus", "LocationCountry"],
-    max_studies=10,
+    fields=["NCTId", "Condition", "BriefTitle", "OverallStatus", "LocationCountry", "MaximumAge", "MinimumAge"],
+    max_studies=3,
     fmt="json",
 )
 
 # Pretty-print the dictionary
-formatted_json = json.dumps(res, indent=4)
+formatted_json = json.dumps(res2, indent=4)
 
 # Print the nicely formatted JSON
 print(formatted_json)
